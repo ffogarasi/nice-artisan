1271a6b18c3a3162ee2aa72b01ac2e4ecfbe4aa6
Fogarasi Ferenc <ferenc.fogarasi@lynxsolutions.eu>
2016-04-04 15:36:07 +0300

    added get call to route

--- src/Http/routes.php
+++ src/Http/routes.php
@@ -3,5 +3,5 @@
 Route::group(['prefix' => 'niceartisan'], function () {
     Route::get('/{option?}', '\FFogarasi\NiceArtisan\Http\Controllers\NiceArtisanController@show');
     Route::post('item/{class}', '\FFogarasi\NiceArtisan\Http\Controllers\NiceArtisanController@command');
-    Route::post('rest_item/{class}', '\FFogarasi\NiceArtisan\Http\Controllers\NiceArtisanController@command');
+    Route::match(['get', 'post'], 'rest_item/{class}', '\FFogarasi\NiceArtisan\Http\Controllers\NiceArtisanController@command');
 });
